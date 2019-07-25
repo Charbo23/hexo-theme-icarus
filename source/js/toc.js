4fdaf27ea15c3ac8cd4f6f0ca1f3d511cc818832
charbo23 <charbo233@qq.com>
2019-07-25 17:33:56 +0800

    目录平滑切换

---
+++ source/js/toc.js
@@ -0,0 +1,6 @@
+$('#toc').on("click", ".toc-item", function() {
+    var target = this.getAttribute("data-href");
+    $('html, body').animate({ scrollTop: $(target).offset().top-20 }, 600);
+    window.location.hash = target;
+    //todo:返回事件无法回到原位置
+});
