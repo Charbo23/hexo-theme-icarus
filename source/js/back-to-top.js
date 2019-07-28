62322c6861c82041605b6e86dcd513059a1a21d1
charbo23 <charbo233@qq.com>
2019-07-28 10:46:12 +0800

    导航栏切换完成

--- source/js/back-to-top.js
+++ source/js/back-to-top.js
@@ -139,10 +139,8 @@ $(document).ready(function () {
             // mobile and tablet mode
             if (!isScrollUp()) {
                 applyState(state['mobile-hidden']);
-                // document.getElementById('navbar-main').classList.remove("is-sticky");
             } else {
                 applyState(state['mobile-visible']);
-                // document.getElementById('navbar-main').classList.add("is-sticky");
             }
             updateScrollTop();
         }
